97349bb8c65ef1a8f733861f34c6cabe62e79113
MeatyTheMeatball <61798980+MeatyTheMeatball@users.noreply.github.com>
2021-12-16 20:23:30 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
-let speed = 24;
+
+let xspeed = 15;
+let yspeed = 15;
 let counter = 0;
 let col = "yellow";
 let bkg = "black";
 let ballX = 40;
+let ballY = ballX*5;
 let rank = "Newbie";
-
+let corners = 0
+let crank = 'No Satisfaction'
 function setup() {
   createCanvas(displayWidth, displayHeight);
+
 }
 
 function draw() {
@@ -14,14 +19,36 @@ function draw() {
   textSize(displayWidth / 15);
   text("Bounces: " + counter, width / 3, height / 5);
   textSize(displayWidth / 30);
-  text("Rank: " + rank, width / width + 20, height - 20);
+  text("Rank: " + rank, width / width + 20, height - 80);
+  text('Corners: ' + corners, width/ 3, height/ 3)
+  text('Corner Rank: ' + crank, width/width + 20, height - 20)
   noStroke();
   fill(col);
-  ellipse(ballX, height / 1.5, 100, 100);
-  ballX = ballX + speed;
+  ellipse(ballX, ballY, 100, 100);
+  ballX = ballX + xspeed;
+  ballY = ballY + yspeed
   if (ballX < 40 || ballX > width - 40) {
     counter = counter + 1;
-    speed = -speed;
+    xspeed = -xspeed;
+  }
+  if (ballY < 40 || ballY > height - 40) {
+    counter = counter + 1;
+    yspeed = -yspeed;
+  }
+  if (ballX <40 && ballY < 40 || ballY > height - 40 && ballX > width - 40 || ballX <40 && ballY > height - 40 || ballX > width - 40 && ballY < 40) {
+    corners = corners + 1
+  }
+  if (corners > 9) {
+    crank = 'A Little Bit Cool'
+  }
+  if (corners > 24) {
+    crank = 'Kinda Awesome'
+  }
+  if (corners > 49) {
+    crank = 'Factual'
+  }
+  if (corners > 99) {
+    crank = 'Very Lucky'
   }
   if (counter > 199) {
     col = "red";
@@ -50,5 +77,13 @@ function draw() {
   if (counter > 9999) {
     col = "brown";
     rank = "No Life";
+  }
+  if (counter > 14999) {
+    col = "grey";
+    rank = "Go Touch Grass";
+}
+  if (counter > 19999) {
+    col = '#513e00'
+    rank = 'No Getting Past This' 
   }
 }
